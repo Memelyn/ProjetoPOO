f54c824fe9da503869dc1b62fb21f8699bbd4379
Mário <mteixeiraaneto@gmail.com>
2024-11-30 22:25:35 -0300

    Create LoginServlet.java

---
+++ src/java/web/LoginServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        // Caso não precise mais do redirecionamento
+        response.getWriter().write("Funcionalidade removida.");
+    }
+}
